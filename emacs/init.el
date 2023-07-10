4a11a95163e03b7c293b51b60424eb9d8cba183b
Ander.Zhao <ander.zhao@outlook.com>
2023-07-10 22:33:13 +0800

    feat(emacs): Remove some packages

--- emacs/init.el
+++ emacs/init.el
@@ -76,13 +76,6 @@
 
 (require 'use-package)
 
-(use-package benchmark-init
-  :load-path "~/.config/emacs/extensions/benchmark-init"
-  :after
-  (require 'benchmark-init-loaddefs)
-  (benchmark-init/activate)
-  :hook (after-init . benchmark-init/deactivate))
-
 (use-package async
   :load-path "~/.config/emacs/extensions/async")
 
@@ -123,9 +116,6 @@
 (use-package magit-todos
   :load-path "~/.config/emacs/extensions/magit-todos")
 
-(use-package org
-  :load-path "~/.config/emacs/extensions/org-mode/lisp/")
-
 (use-package ivy
   :load-path "~/.config/emacs/extensions/swiper"
   :hook (after-init . ivy-mode)
@@ -135,6 +125,7 @@
 	enable-recursive-minibuffers t
         ivy-count-format "%d/%d "
         ivy-re-builders-alist '((t . ivy--regex-ignore-order))))
+
 (use-package counsel
   :load-path "~/.config/emacs/extensions/swiper"
   :after (ivy)
@@ -142,6 +133,7 @@
          ("C-x C-f" . counsel-find-file)
          ("C-c f"   . counsel-recentf)
          ("C-c g"   . counsel-git)))
+
 (use-package swiper
   :load-path "~/.config/emacs/extensions/swiper"
   :after (ivy)
@@ -160,8 +152,7 @@
         company-idle-delay 0.2
         company-minimum-prefix-length 1
         company-show-quick-access t))
-(use-package flymake
-  :hook (prog-mode . flymake-mode))
+
 (use-package ace-window
   :load-path "~/.config/emacs/extensions/ace-window"
   :hook (prog-mode . ace-window-display-mode)
@@ -170,18 +161,22 @@
   (aw-scope 'frame)
   (aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
   (aw-minibuffer-flag t))
+
 (use-package default-text-scale
   :load-path "~/.config/emacs/extensions/default-text-scale"
   :custom
   (default-text-scale-mode)
   :bind (("C-M-=" . default-text-scale-increase)
          ("C-M--" . default-text-scale-decrease)))
+
 (use-package move-dup
   :load-path "~/.config/emacs/extensions/move-dup"
   :hook (prog-mode . global-move-dup-mode))
+
 (use-package evil-nerd-commenter
   :load-path "~/.config/emacs/extensions/evil-nerd-commenter"
   :bind ("M-/" . evilnc-comment-or-uncomment-lines))
+
 (use-package ws-butler
   :load-path "~/.config/emacs/extensions/ws-butler"
   :hook
@@ -198,11 +193,6 @@
   :config (which-key-mode 1)
   (setq which-key-idle-delay 0.3))
 
-(use-package alert
-  :load-path "~/.config/emacs/extensions/alert"
-  :config
-  (setq alert-default-style 'notifications))
-
 (use-package projectile
   :load-path "~/.config/emacs/extensions/projectile"
   :commands (projectile-mode)
@@ -211,28 +201,16 @@
   (projectile-mode 1)
   (define-key projectile-mode-map (kbd "C-c C-p") 'projectile-command-map))
 
-(use-package lsp-mode
-  :load-path "~/.config/emacs/extensions/lsp-mode"
-  :commands (lsp lsp-deferred)
-  :init
-  (setq lsp-keymap-prefix "c-c 1")
-  :config
-  (1sp-enable-which-key-integration t))
-
-(use-package lsp-ui
-  :load-path "~/.config/emacs/extensions/lsp-ui"
-  :after (lsp-mode)
-  :commands (lsp-ui-mode)
-  :hook (lsp-mode . lsp-ui-mode))
-
 ;; Templates
 (use-package yasnippet
   :load-path "~/.config/emacs/extensions/yasnippet"
   :commands (yas-global-mode)
   :config (yas-global-mode 1))
+
 (use-package yasnippet-snippets
   :load-path "~/.config/emacs/extensions/yasnippet-snippets"
   :after (yasnippet))
+
 (use-package auto-yasnippet
   :load-path "~/.config/emacs/extensions/auto-yasnippet"
   :bind (("C-c & w" . aya-create)
@@ -240,48 +218,13 @@
   :config
   (setq aya-persist-snippets-dir (concat user-emacs-directory "/snippets")))
 
-
-(use-package anzu
-  :load-path "~/.config/emacs/extensions/anzu")
-
 (use-package multiple-cursors
   :load-path "~/.config/emacs/extensions/multiple-cursors"
-  :bind (("C-S-c" . mc/edit-lines) ;; 每行一个光标
-         ("C->" . mc/mark-next-like-this-symbol) ;; 全选光标所在单词并在下一个单词增加一个光标。通常用来启动一个流程
-         ("C-M->" . mc/skip-to-next-like-this) ;; 跳过当前单词并跳到下一个单词，和上面在同一个流程里。
-         ("C-<" . mc/mark-previous-like-this-symbol) ;; 同样是开启一个多光标流程，但是是「向上找」而不是向下找。
-         ("C-M-<" . mc/skip-to-previous-like-this) ;; 跳过当前单词并跳到上一个单词，和上面在同一个流程里。
-         ("C-c C->" . mc/mark-all-symbols-like-this))) ;; 直接多选本 buffer 所有这个单词
-
-(use-package vterm
-  :load-path "~/.config/emacs/extensions/emacs-libvterm"
-  :config
-  (setq vterm-kill-buffer-on-exit t))
-
-(use-package edwina
-  :load-path "~/.config/emacs/extensions/edwina"
-  :commands (edwina-setup-dwm-keys edwina-mode)
-  :config
-  ;; 让所有 display-buffer 动作都新增一个 window （而不是复用已经打开此 buffer 的 window）
-  (setq display-buffer-base-action '(display-buffer-below-selected))
-  ;; 以下定义会被 (edwina-setup-dwm-keys) 增加 'M-' 修饰。
-  ;; 我自定义了一套按键，因为原版会把我很常用的 M-d 覆盖掉。
-  (setq edwina-dwm-key-alist
-        '(("r" edwina-arrange)
-          ("j" edwina-select-next-window)
-          ("k" edwina-select-previous-window)
-          ("J" edwina-swap-next-window)
-          ("K" edwina-swap-previous-window)
-          ("h" edwina-dec-mfact)    ;; 主窗口缩窄
-          ("l" edwina-inc-mfact)    ;; 主窗口拉宽
-          ("D" edwina-dec-nmaster)  ;; 减少主窗口的数量
-          ("I" edwina-inc-nmaster)  ;; 增加主窗口的数量
-          ("C" edwina-delete-window) ;; 关闭窗口
-          ("RET" edwina-zoom t)     ;; 交换「主窗口」和「副窗口」
-          ("return" edwina-zoom t)
-          ("S-RET" edwina-clone-window t) ;; 复制一个本窗口
-          ("S-return" edwina-clone-window t)))
-  (edwina-setup-dwm-keys)
-  (edwina-mode 1))
+  :bind (("C-S-c" . mc/edit-lines)
+         ("C->" . mc/mark-next-like-this-symbol)
+         ("C-M->" . mc/skip-to-next-like-this)
+         ("C-<" . mc/mark-previous-like-this-symbol)
+         ("C-M-<" . mc/skip-to-previous-like-this)
+         ("C-c C->" . mc/mark-all-symbols-like-this)))
 
 ;;; init.el ends here
